bcff5278c7bb5431aff46532ec398b6766db485e
reneferg <rene.ferguson@intel.com>
2020-06-05 01:47:05 -0700

    complicated search

--- search.py
+++ search.py
@@ -21,35 +21,89 @@ def search_dataset(first, second):
     res = vc[vc > 1].index.tolist()
 
     if len(res) < 1:
-        print('No pokemon have access to both moves.')
-        print(first, "is learnt by:", f_pokes)
-        print(second, "is learnt by:", s_pokes)
+        return False, [f_pokes, s_pokes]
     else:
-        print('These pokemon can learn both moves:', vc[vc > 1].index.tolist())
+        return True, [res]
 
 
+def search_all(list_of_moves):
+    vc = list()
+    for move in list_of_moves:
+        filtered = moves_data[moves_data['Move'] == move.upper()]['Pokemon'].values[0]
+        vc = vc + filtered
+    vc = pd.Series(vc).value_counts()
+    return vc[vc >= len(list_of_moves)].index.tolist()
 
+
+def search_all_of_and_any(all_list, any_list):
+    all_result = search_all(all_list)
+    matches = pd.DataFrame(columns=['Pokemon', 'Move'])
+    for move in any_list:
+        try:
+            pokes = moves_data[moves_data['Move'] == move.upper()]['Pokemon'].values[0]
+            vc = pd.Series(pokes + all_result).value_counts()
+            df = pd.DataFrame({'Pokemon': vc[vc > 1].index, 'Move': move})
+            if len(df) > 0:
+                matches = matches.append(df, ignore_index=True).sort_values('Pokemon')
+        except IndexError:
+            print('No move found matching', move)
+    return matches
+
+
+def search_multi_any(all_list, any_lists):
+    sub_matches = list()
+    for li in any_lists:
+        sub_matches.append(search_all_of_and_any(all_list, li))
+
+    pokes = list()
+    for df in sub_matches:
+        pokes = pokes + df['Pokemon'].unique().tolist()
+
+    if len(pokes) > 0:
+        pokes = pd.Series(pokes).value_counts()
+        print(pokes)
+        pokes = pokes[pokes >= len(any_lists)].index.tolist()
+        guide = pd.DataFrame()
+        guide['Pokemon'] = pokes
+        for i in range(0, len(sub_matches)):
+            col_name = 'Move Set ' + str(i + 1)
+            df = sub_matches[i]
+            guide.insert(i+1, col_name, guide['Pokemon'].apply(lambda x: df[df['Pokemon'] == x]['Move'].tolist()))
+        return guide
+    else:
+        print('No pokemon found matching the requirements.')
+        return sub_matches
+
+
+def search_of_any(first, list_of_moves):
+    guide = pd.DataFrame(columns=['Pokemon', 'Move'])
+    for move in list_of_moves:
+        res, lists = search_dataset(first, move)
+        if res:
+            df = pd.DataFrame({'Pokemon': lists[0], 'Move': move})
+            guide = guide.append(df, ignore_index=True).sort_values('Pokemon')
+    return guide
 
 
 moves_data = pd.read_csv('latest.csv', header=0)
 
 moves_data['Pokemon'] = moves_data['Pokemon'].apply(lambda x: ast.literal_eval(x))
 moves_data['Move'] = moves_data['Move'].apply(lambda x: str(x).upper())
-
-parser = argparse.ArgumentParser()
-parser.add_argument('first')
-parser.add_argument('second')
-
-parsed_args = parser.parse_args()
-x = parsed_args.first
-y = parsed_args.second
-
-
-while True:
-    search_dataset(x, y)
-    input('Press a key to search again')
-    replace = int(input('Which move to replace? 0 (both), 1 (first), 2 (second)'))
-    if replace is 0 or replace is 1:
-        x = str(input('New First Move?'))
-    if replace is 0 or replace is 2:
-        y = str(input('New Second move?'))
+pd.set_option('display.max_columns', 500)
+# parser = argparse.ArgumentParser()
+# parser.add_argument('first')
+# parser.add_argument('second')
+#
+# parsed_args = parser.parse_args()
+# f = parsed_args.first
+# s = parsed_args.second
+#
+#
+# while True:
+#     search_dataset(f, s)
+#     input('Press a key to search again')
+#     replace = int(input('Which move to replace? 0 (both), 1 (first), 2 (second)'))
+#     if replace is 0 or replace is 1:
+#         f = str(input('New First Move?'))
+#     if replace is 0 or replace is 2:
+#         s = str(input('New Second move?'))
